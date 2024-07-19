8cb32d1aa5ffd61d92e8c6dc790d1167abd94b7c
lunacu <36743848+lunacu@users.noreply.github.com>
2024-07-19 14:15:39 -0300

    update modelos 2
    update modelos 2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     
-    if (username === "admin" && password === "admin" || username === "jmartinez" && password === "jmartinez") {
+    if ((username === "admin" && password === "admin") || (username === "jmartinez" && password === "jmartinez")) {
         window.location.href = "modelos/modelos.html";
     } else {
         alert("Usuario o contraseña incorrectos");
