8942ba4315b63c1d018f94a3fc54be9d3eb57c73
lunacu <36743848+lunacu@users.noreply.github.com>
2024-07-19 14:03:12 -0300

    modificacion para hacerlo site
    modificacion para hacerlo site

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    if (username === "admin" && password === "admin") {
+        window.location.href = "modelos/modelos.html";
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
+});
